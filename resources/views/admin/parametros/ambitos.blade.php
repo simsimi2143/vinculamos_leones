0d7df259a040f7a0524b74968ed4446f35ca00b1
jorge-conejeros <jorgeconejerosrios@gmail.com>
2023-08-21 19:39:07 -0400

    feat: Cambio de una letra en Contribucion

--- resources/views/admin/parametros/ambitos.blade.php
+++ resources/views/admin/parametros/ambitos.blade.php
@@ -151,10 +151,10 @@
                                     <select class="form-control @error('descripcion') is-invalid @enderror" id="descripcion"
                                         name="descripcion">
                                         <option value="" selected disable d>Seleccione...</option>
-                                        <option value="Contribución Interno"
+                                        <option value="Contribución Interna"
                                             {{ 'Contribución Interna' == $ambi->amb_descripcion ? 'selected' : '' }}>Contribución
                                             Interna</option>
-                                        <option value="Contribución Externo"
+                                        <option value="Contribución Externa"
                                             {{ 'Contribución Externa' == $ambi->amb_descripcion ? 'selected' : '' }}>Contribución
                                             Externa</option>
                                     </select>
